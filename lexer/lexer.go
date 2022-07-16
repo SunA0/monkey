b42ee8caf975634a5cc57c107827c54db7abe5f4
Sun <2534658059@qq.com>
2022-07-16 11:59:15 +0800

    feat：define lexical units

---
+++ lexer/lexer.go
@@ -0,0 +1,56 @@
+package lexer
+
+import "monkey/token"
+
+type lexer struct {
+	input        string
+	position     int  // 当前字符串的位置
+	readPosition int  // 当前字符串的位置的下一个位置，读取位置
+	ch           byte // 当前字符
+}
+
+func NewLexer(input string) *lexer {
+	l := &lexer{input: input}
+	l.readChar()
+	return l
+}
+
+// readChar
+// 读取input中下一个字符
+func (l *lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition += 1
+}
+
+func (l *lexer) NextToken() token.Token {
+	var tok token.Token
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	}
+	l.readChar()
+	return tok
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
+}
